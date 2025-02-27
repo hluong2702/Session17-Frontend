4f7ab50a5df490881294c5aadad6d50a872b95e2
hluong2702 <hoangluong2702@gmail.com>
2025-02-27 19:30:34 +0700

    Update

--- Session17ex9.js
+++ Session17ex9.js
@@ -1,19 +1,19 @@
-let a = +prompt('Nhập giá trị a:');
-let b = +prompt('Nhập giá trị b:');
-let c = +prompt('Nhập giá trị c:');
+let a = +prompt('Nhap gia tri a:');
+let b = +prompt('Nhap gia tri b:');
+let c = +prompt('Nhap gai tri c:');
 
 let delta = b * b - 4 * a * c;
 if(a !=0){
     if (delta > 0 ) {
     let x1 = (-b + Math.sqrt(delta)) / (2 * a);
     let x2 = (-b - Math.sqrt(delta)) / (2 * a);
-    document.write(`Phương trình có hai nghiệm phân biệt: x1 = ${x1}, x2 = ${x2}`);
+    document.write(`Phuong trinh co 2 nghiem phan biet: x1 = ${x1}, x2 = ${x2}`);
     } else if (delta === 0) {
     let x1 = -b / (2 * a);
-    document.write(`Phương trình có nghiệm kép: x1 = x2 = ${x1}`);
+    document.write(`Phuong trinh co nghiem kep: x1 = x2 = ${x1}`);
     } else {
-    document.write(`Phương trình vô nghiệm`);
+    document.write(`Phuong trinh vo nghiem`);
     }
 }else{
-    document.write(`Phương trình bậc nhất`);
+    document.write(`Phuong trinh bac nhat`);
 }
